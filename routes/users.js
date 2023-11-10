80c29793d4b02a233e81e7273e9979df4c5f5b6b
James Wang <114769089+jameswang08@users.noreply.github.com>
2023-11-10 00:10:25 -0800

    Display env var on web page to test if working

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(process.env.envTest);
 });
 
 module.exports = router;
